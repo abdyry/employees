f79a03bb2e7694bc4e8c700c7e2617b8180b34c3
abdi <abdiabdirizakomar@gmail.com>
2024-01-19 20:13:17 +0300

    awesome commit 2

--- resources/views/reports/params/em.blade.php
+++ resources/views/reports/params/em.blade.php
@@ -1,7 +1,6 @@
 <div class="max-w-md mx-auto mt-8 bg-white dark:bg-gray-800 p-6 rounded-md shadow-md">
-    <h2 class="text-2xl font-semibold mb-4 text-gray-800 dark:text-white">Customer Report Parameters</h2>
+    <h2 class="text-2xl font-semibold mb-4 text-gray-800 dark:text-white">Employee Report Parameters</h2>
 
-    
     <div class="mb-4">
         <label for="employeeName" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Employee Name</label>
         <input type="text" id="employeeName" name="employeeName" class="mt-1 p-2 w-full border rounded-md">
